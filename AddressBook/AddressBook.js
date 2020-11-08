6bcdb1d474d7e6b8ee6c36454822bb62b86c9273
madhu179 <madhubabu232343@gmail.com>
2020-11-08 19:27:08 +0530

    Adding code to create AddressBook contact with given fields

---
+++ AddressBook/AddressBook.js
@@ -0,0 +1,34 @@
+class Contact{
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phone = params[6];
+        this.email = params[7];
+    }
+
+
+    toString()
+    {
+        return "FirstName = "+this.firstName+", LastName = "+this.lastName+", Address = "+this.address+
+                ", City = "+this.city+", State = "+this.state+", Zip = "+this.zip+", Phone = "+this.phone+", Email = "+this.email;
+    }
+
+}
+
+let contact = new Contact("Tony","Stark","10880 Malibu Point 90265","New York City","New York",10001,9876432387,"TonyStark@Yahoo.com");
+console.log(contact.toString());
